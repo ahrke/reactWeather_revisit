b275eefee8c16d2801147fb63f118d602392b5d5
ahrke <david.duy.minh@gmail.com>
2017-09-27 21:46:34 +0700

    server.js...once more

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use(function (req, res, next){
 
 app.use(express.static('./public'));
 
-app.listen(req, res, function() {
+app.listen(PORT, function() {
 	console.log('express server is up on port ' + PORT);
 });
