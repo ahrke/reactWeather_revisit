6204dd5f5116f90f49162f6de62c86fc24628b79
ahrke <david.duy.minh@gmail.com>
2017-09-27 21:43:22 +0700

    changed server.js to adjust .listen

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use(function (req, res, next){
 
 app.use(express.static('./public'));
 
-app.listen(3000, function() {
+app.listen(req, res, function() {
 	console.log('express server is up on port ' + PORT);
 });
